fe82909a1248469b5cc75c0e998a5eac3d32e21f
darlingson <dmakuwila@gmail.com>
2024-07-31 20:07:44 +0200

    created a sample soil structure obj

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 import Fastify from 'fastify'
+const soilStructure = {
+    blantyre: {
+        soilType: "Sandy loam",
+        suitableCrops: ["Maize", "Groundnuts", "Tobacco"]
+    },
+    lilongwe: {
+        soilType: "Clay loam",
+        suitableCrops: ["Maize", "Soybeans", "Tobacco"]
+    },
+    mzuzu: {
+        soilType: "Sandy clay loam",
+        suitableCrops: ["Tea", "Coffee", "Macadamia"]
+    }
+};
 const fastify = Fastify({
     logger: true
 })
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
+})
+fastify.get('/location/:name',async()=>{
+  //check if name is in soilStructure
+  if(soilStructure[name]){
+      return soilStructure[name]
+  }
+  else{
+      return {
+          soilType: "Unknown",
+          suitableCrops: ["Unknown"]
+      }
+  }
 })
 const start = async () => {
     try {
